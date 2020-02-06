dcd3e01f3d0aa8502f2739780232658e13eaefe8
Bryant-Reynoso <52264330+Bryant-Reynoso@users.noreply.github.com>
2020-02-06 13:35:39 -0600

    added MdController and some logic

---
+++ ViewzApi/Controllers/MDController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ViewzApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MDController : ControllerBase
+    {
+        [HttpPost] 
+        public ContentResult Post([FromForm]string MD)
+        { 
+            string output = "";
+
+            if (MD.Contains("#"))
+            { 
+                output = $"<h1>{MD}</h1>";
+            }
+            else
+            {
+                output = MD;
+            }
+
+            return base.Content(output, "text/html");
+            
+        }
+         
+    }
+}
