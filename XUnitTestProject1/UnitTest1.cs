8db079f5b87111651a4c8aa62b3cab65635bdeb4
Jacob Fisher <jacobdf@cs.unc.edu>
2020-02-07 20:46:32 -0600

    Changed Tests to suit requirements for project

--- XUnitTestProject1/UnitTest1.cs
+++ XUnitTestProject1/UnitTest1.cs
@@ -10,16 +10,28 @@ namespace XUnitTestProject1
         {
 
             //Arrange
-            string content = "a woiejal lkwjeoije aiuoiwuya yowj <a href=\"Page1\">okay so this is a link</a> href=\"Page2\" ok so this is not... but it still works.";
+            string content = "a woiejal lkwjeoije aiuoiwuya yowj <a href=\"Page1\">okay so this is a link</a> href=\"Page2\" This should remain the same";
             string to_replace = "href=\"";
             string replace_with = "href=\"Controller/Action/";
-            string Expected = "a woiejal lkwjeoije aiuoiwuya yowj <a href=\"Controller/Action/Page1\">okay so this is a link</a> href=\"Controller/Action/Page2\" ok so this is not... but it still works.";
+            string Expected = "a woiejal lkwjeoije aiuoiwuya yowj <a href=\"Controller/Action/Page1\">okay so this is a link</a> href=\"Page2\" This should remain the same";
 
             //Act
             string Actual = Methods.Parsing.ReplaceInside(content, to_replace, replace_with);
 
             //Assert
             Assert.Equal(Expected, Actual);
+        }
+        [Fact]
+        public void DontReplaceLinks()
+        {
+            string content = "this is an <a href=\"https://www.google.com/\">Test</a> anchor to google.com";
+            string to_replace = "href=\"";
+            string replace_with = "href=\"Controller/Action/";
+            string expected = "this is an <a href=\"https://www.google.com/\">Test</a> anchor to google.com";
+
+            string actual = Methods.Parsing.ReplaceInside(content, to_replace, replace_with);
+
+            Assert.Equal(expected, actual);
         }
     }
 }
