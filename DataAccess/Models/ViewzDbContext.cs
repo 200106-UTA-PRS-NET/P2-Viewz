1df90d90cf1d9fab1316b24d1c46c6eaf8365c19
Jacob Fisher <jacobdf@cs.unc.edu>
2020-02-08 09:06:51 -0600

    Separated the content from the pages, so that we won't be loading both md and html from the db every time we get an object. Added image table if we want to work with images later.

--- DataAccess/Models/ViewzDbContext.cs
+++ DataAccess/Models/ViewzDbContext.cs
@@ -16,9 +16,14 @@ namespace DataAccess.Models
         }
 
         public virtual DbSet<Contents> Contents { get; set; }
+        public virtual DbSet<Images> Images { get; set; }
         public virtual DbSet<Page> Page { get; set; }
         public virtual DbSet<PageDetails> PageDetails { get; set; }
+        public virtual DbSet<PageHtmlContent> PageHtmlContent { get; set; }
+        public virtual DbSet<PageMdContent> PageMdContent { get; set; }
         public virtual DbSet<Wiki> Wiki { get; set; }
+        public virtual DbSet<WikiHtmlDescription> WikiHtmlDescription { get; set; }
+        public virtual DbSet<WikiMdDescription> WikiMdDescription { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
@@ -54,6 +59,31 @@ namespace DataAccess.Models
                     .HasConstraintName("FK__contents__pageId__5EBF139D");
             });
 
+            modelBuilder.Entity<Images>(entity =>
+            {
+                entity.HasKey(e => new { e.WikiId, e.ImageName })
+                    .HasName("PK__images__00EE579B7814E494");
+
+                entity.ToTable("images", "wiki");
+
+                entity.Property(e => e.WikiId).HasColumnName("wikiId");
+
+                entity.Property(e => e.ImageName)
+                    .HasColumnName("imageName")
+                    .HasMaxLength(255)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Image)
+                    .IsRequired()
+                    .HasColumnName("image");
+
+                entity.HasOne(d => d.Wiki)
+                    .WithMany(p => p.Images)
+                    .HasForeignKey(d => d.WikiId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK__images__wikiId__73BA3083");
+            });
+
             modelBuilder.Entity<Page>(entity =>
             {
                 entity.ToTable("page", "wiki");
@@ -63,10 +93,6 @@ namespace DataAccess.Models
                     .IsUnique();
 
                 entity.Property(e => e.PageId).HasColumnName("pageId");
-
-                entity.Property(e => e.HtmlContent).HasColumnType("ntext");
-
-                entity.Property(e => e.MdContent).HasColumnType("ntext");
 
                 entity.Property(e => e.PageName).HasColumnType("ntext");
 
@@ -111,6 +137,46 @@ namespace DataAccess.Models
                     .HasConstraintName("FK_WikiPage");
             });
 
+            modelBuilder.Entity<PageHtmlContent>(entity =>
+            {
+                entity.HasKey(e => e.PageId)
+                    .HasName("PK__pageHtml__54B1FF747131F7EC");
+
+                entity.ToTable("pageHtmlContent", "wiki");
+
+                entity.Property(e => e.PageId)
+                    .HasColumnName("pageId")
+                    .ValueGeneratedNever();
+
+                entity.Property(e => e.HtmlContent).HasColumnType("ntext");
+
+                entity.HasOne(d => d.Page)
+                    .WithOne(p => p.PageHtmlContent)
+                    .HasForeignKey<PageHtmlContent>(d => d.PageId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK__pageHtmlC__pageI__68487DD7");
+            });
+
+            modelBuilder.Entity<PageMdContent>(entity =>
+            {
+                entity.HasKey(e => e.PageId)
+                    .HasName("PK__pageMdCo__54B1FF748DFBB833");
+
+                entity.ToTable("pageMdContent", "wiki");
+
+                entity.Property(e => e.PageId)
+                    .HasColumnName("pageId")
+                    .ValueGeneratedNever();
+
+                entity.Property(e => e.MdContent).HasColumnType("ntext");
+
+                entity.HasOne(d => d.Page)
+                    .WithOne(p => p.PageMdContent)
+                    .HasForeignKey<PageMdContent>(d => d.PageId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK__pageMdCon__pageI__656C112C");
+            });
+
             modelBuilder.Entity<Wiki>(entity =>
             {
                 entity.ToTable("wiki", "wiki");
@@ -119,15 +185,51 @@ namespace DataAccess.Models
                     .HasName("UQ__wiki__C5B214316D7F0DFB")
                     .IsUnique();
 
-                entity.Property(e => e.DescriptionHtml).HasColumnType("ntext");
-
-                entity.Property(e => e.DescriptionMd).HasColumnType("ntext");
-
                 entity.Property(e => e.PageName).HasColumnType("ntext");
 
                 entity.Property(e => e.Url)
                     .HasMaxLength(255)
                     .IsUnicode(false);
+            });
+
+            modelBuilder.Entity<WikiHtmlDescription>(entity =>
+            {
+                entity.HasKey(e => e.WikiId)
+                    .HasName("PK__wikiHtml__54B1FF7462781F78");
+
+                entity.ToTable("wikiHtmlDescription", "wiki");
+
+                entity.Property(e => e.WikiId)
+                    .HasColumnName("wikiId")
+                    .ValueGeneratedNever();
+
+                entity.Property(e => e.HtmlDescription).HasColumnType("ntext");
+
+                entity.HasOne(d => d.Wiki)
+                    .WithOne(p => p.WikiHtmlDescription)
+                    .HasForeignKey<WikiHtmlDescription>(d => d.WikiId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK__wikiHtmlD__pageI__6E01572D");
+            });
+
+            modelBuilder.Entity<WikiMdDescription>(entity =>
+            {
+                entity.HasKey(e => e.WikiId)
+                    .HasName("PK__wikiMdDe__54B1FF744921A12E");
+
+                entity.ToTable("wikiMdDescription", "wiki");
+
+                entity.Property(e => e.WikiId)
+                    .HasColumnName("wikiId")
+                    .ValueGeneratedNever();
+
+                entity.Property(e => e.MdDescription).HasColumnType("ntext");
+
+                entity.HasOne(d => d.Wiki)
+                    .WithOne(p => p.WikiMdDescription)
+                    .HasForeignKey<WikiMdDescription>(d => d.WikiId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK__wikiMdDes__pageI__6B24EA82");
             });
 
             OnModelCreatingPartial(modelBuilder);
