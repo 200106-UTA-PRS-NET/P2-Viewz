8adf8c6d61ddad3dbd8d198d02d887ce2b28ed6c
Bryant-Reynoso <52264330+Bryant-Reynoso@users.noreply.github.com>
2020-02-08 19:00:35 -0600

    made mockrepository public

--- DataAccess/MockRepositories/MockPageRepository.cs
+++ DataAccess/MockRepositories/MockPageRepository.cs
@@ -6,7 +6,7 @@ using System.Text;
 namespace DataAccess.MockRepositories
 {
 
-    class MockPageRepository : IPageRepository
+    public class MockPageRepository : IPageRepository
     {
         class Page
         {
