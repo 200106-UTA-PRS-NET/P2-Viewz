28239f0d6548f8fa150e7c5ee4aff7401fbe14ac
Bryant-Reynoso <52264330+Bryant-Reynoso@users.noreply.github.com>
2020-02-12 12:35:06 -0600

    Modified page model in api

--- ViewzApi/Models/Page.cs
+++ ViewzApi/Models/Page.cs
@@ -8,22 +8,31 @@ namespace ViewzApi.Models
 {
     public class Page
     {
-        public Page()
-        {
-            Contents = new HashSet<Contents>();
-            PageDetails = new HashSet<PageDetails>();
-        }
+        //public Page()
+        //{
+        //    Contents = new HashSet<Contents>();
+        //    PageDetails = new HashSet<PageDetails>();
+        //}
 
-        //public int WikiId { get; set; }
-        //public long PageId { get; set; }
+        ////public int WikiId { get; set; }
+        ////public long PageId { get; set; }
+        //public string Url { get; set; }
+        //public string PageName { get; set; }
+
+        //public string Content { get; set; }
+        ////public virtual Wiki Wiki { get; set; }
+        ////public virtual PageHtmlContent PageHtmlContent { get; set; }
+        ////public virtual PageMdContent PageMdContent { get; set; }
+        //public virtual ICollection<Contents> Contents { get; set; }
+        //public virtual ICollection<PageDetails> PageDetails { get; set; }
+
+
+        public string WikiUrl { get; set; }
         public string Url { get; set; }
         public string PageName { get; set; }
- 
-        public string Content { get; set; }
-        //public virtual Wiki Wiki { get; set; }
-        //public virtual PageHtmlContent PageHtmlContent { get; set; }
-        //public virtual PageMdContent PageMdContent { get; set; }
-        public virtual ICollection<Contents> Contents { get; set; }
-        public virtual ICollection<PageDetails> PageDetails { get; set; }
+        public string MdContent { get; set; }
+        public string HtmlContent { get; set; }
+        public IEnumerable<Contents> Contents { get; set; }
+        public IEnumerable<PageDetails> Details { get; set; }
     }
 }
