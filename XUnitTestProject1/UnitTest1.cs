1741f11bfc9692b99a9fa89e3d3bea1e7f171602
Jacob Fisher <jacobdf@cs.unc.edu>
2020-02-12 19:45:36 -0600

    Added super simple tests to see how azure handles them

--- XUnitTestProject1/UnitTest1.cs
+++ XUnitTestProject1/UnitTest1.cs
@@ -1,10 +1,30 @@
 //using System;
-//using Xunit;
+using DataAccess.Storing;
+using Xunit;
 //using DataAccess.APIAccess;
 
 namespace XUnitTestProject1
 {
     public class UnitTest1
     {
+        [Theory]
+        [InlineData("page-name")]
+        public void SimpleTest(string name)
+        {
+            Page page = new Page() { 
+                PageName = name
+            };
+            Assert.Equal(name, page.PageName);
+        }
+        [Theory]
+        [InlineData("page-name")]
+        public void SimpleFailingTest(string name)
+        {
+            Page page = new Page()
+            {
+                PageName = name
+            };
+            Assert.NotEqual(name, page.PageName);
+        }
     }
 }
