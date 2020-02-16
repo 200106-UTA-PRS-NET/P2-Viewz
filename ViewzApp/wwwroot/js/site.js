415ee071507b24910f71b0d8baba4b14fac669e8
redalgorithm <shaanancurtis@ieee.org>
2020-02-15 22:50:20 -0600

    update

--- ViewzApp/wwwroot/js/site.js
+++ ViewzApp/wwwroot/js/site.js
@@ -2,3 +2,7 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+function hell_o() {
+    document.getElementById("what").innerHTML = "WHAT";
+}
+hell_o();
